df1bd238f1752463033af7976e1de3d9be91da48
PapeMor <ndiayepm@ept.sn>
2024-02-11 15:52:18 +0000

    [Fix Conflit]

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GoogleMap from './components/mapCard/mapCard';
 import GoogleMap1 from './components/mapCard/mapCard1';
 
 import Topbar from "./components/topbar/Topbar";
 import MapPage from './pages/mapPage/mapPage';
 import MapPage1 from './pages/mapPage/mapPage1';
-import InfoPage from './pages/InfoDepot';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-        <Route path="/" element={ <MapPage1/>} />
-        <Route path="/info" element={ <InfoPage/>} />
-        </Routes>
-         
+    <div className="App">
+      <MapPage1/>      
     </div>
-    </Router>
-    
   );
 }
 
